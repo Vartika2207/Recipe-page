6ad0f9101f798bd1ec3054d95500f13b000ceaed
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2021-10-11 04:19:49 +0530

    Added first commit
    First recipe app page

---
+++ Recipe App/script.js
@@ -0,0 +1,75 @@
+const meals =document.getElementById('meals');
+
+
+
+
+
+
+
+
+
+
+//asysnc make the function return promise
+//await before a function makes the function wait for a promise
+getRandomMeal();
+async function getRandomMeal(){
+    const randomMeal_fetched = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+    const respData = await randomMeal_fetched.json();
+    const randomMeal = respData.meals[0];
+    console.log(randomMeal);
+
+    addMeal(randomMeal, true);
+}
+
+async function getMealBySearch(id){
+    const meal = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+}
+
+async function getMealsBySearch(term){
+    const meal = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + term);
+}
+
+function addMeal(mealData, random = false){
+    const meal = document.createElement('div');
+    meal.classList.add('meal');
+    meal.innerHTML = `
+                <div class="meal-header">
+                    ${random ? `
+                    <span class="random">Randome Recipe</span>` 
+                : ``};
+                    <img src="${mealData.strMealThumb}" alt="">
+                </div>
+                <div class="meal-body">
+                    <h4>Veggie Veggies</h4>
+                    <button class="icons">
+                        <i class="far fa-heart"></i>
+                        <i class="far fa-comment"></i>
+                        <i class="far fa-share-square"></i>
+                    </button>
+                </div>
+            `
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
